1905dce4ce442fc3acd0b2fc110161b68645b116
Fredes Virginia <94409602+FredesVirginia@users.noreply.github.com>
2024-03-17 03:01:30 +0000

    diseño listo

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>
-      <h2> Pagina ha2 </h2>   
+    <div class="fondo">
+     
+     <div className="flex justify-center items-center h-screen">
+      <div className="bg-white py-10 w-[400px] rounded-lg">
+        <form className="flex flex-col space-y-3 px-16 ">
+          <h2 className="text-center text-2xl font-bold mb-5 text-gray-700">Twilvo Test-SMS</h2>  
+          <label className='text-gray-700'>Número</label>
+          <input type="text" className="border-2 border-sky-200" />
+
+          <label className='text-gray-700' >Número</label>
+          <input type="text" className="border-2 border-sky-200" />
+
+          <button className="bg-sky-300 text-center p-3">Enviar SMS</button>
+        </form>
+      </div>
+    </div>
     </div>
   )
 }
